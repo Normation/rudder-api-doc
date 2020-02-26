f189695e7f3e84d5eed957f9d82a2e75f314817a
Alexis Mousset <alexis.mousset@rudder.io>
2020-02-26 10:53:01 +0000

    Missing request type in remote-run example

--- src/relay/code_samples/curl/remote-run/node.sh
+++ src/relay/code_samples/curl/remote-run/node.sh
@@ -1,1 +1,1 @@
-curl --request https://rudder.example.com/rudder/relay-api/remote-run/nodes/2fc7a3a2-4050-4209-afe9-1c5ebc9ce2c8 --data keep_output=true
+curl --request POST https://rudder.example.com/rudder/relay-api/remote-run/nodes/2fc7a3a2-4050-4209-afe9-1c5ebc9ce2c8 --data keep_output=true
